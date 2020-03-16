223b8db4f47502718f2115fd703dce7455616fed
Franz Kohnle <franz@kohnlehome.de>
2020-03-16 13:11:16 +0100

    Create model

--- model.js
+++ model.js
@@ -1,10 +1,24 @@
 export class Model {
   constructor() {
-    console.log('Model');
+    this.personen = [];
   }
 
   // CRUD-Methoden
+  // CREATE
+  createPerson(person) {
+    this.personen.push(person);
+  }
+
+  // READ
+  readAllPersons() {
+    return this.personen;
+  }
+
   getText() {
     return 'Hello from Model!';
   }
+
+  // UPDATE
+
+  // DELETE
 }
---
+++ person.js
@@ -0,0 +1,7 @@
+export class Person {
+  constructor() {
+    this.vorname = 'Max';
+    this.nachame = 'Mustermann';
+    this.geburtsdatum = '1999-12-24';
+  }
+}
